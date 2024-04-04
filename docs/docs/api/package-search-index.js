4f6db6d59d19c1cda15f325ce188cbb9faeea2e7
Brian Osborn <bosborn@gmail.com>
2024-04-04 11:45:30 -0600

    1.2.3 Javadoc

--- docs/docs/api/package-search-index.js
+++ docs/docs/api/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"mil.nga.gars.grid.style"},{"l":"mil.nga.gars.tile"}]
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"mil.nga.gars.grid.style"},{"l":"mil.nga.gars.tile"}];updateSearchResults();
